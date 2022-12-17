bee20324477a08dc1d69e1cd7cd478e7e64a57a4
A.V.9.4 <120322748+Acafac1994@users.noreply.github.com>
2022-12-17 18:41:31 +0100

    while schleife mit erklärung

---
+++ main.js
@@ -0,0 +1,17 @@
+function main () {
+
+var coundwown = 0;
+
+while (coundwown < 100){
+    console.log(coundwown)
+    coundwown++;
+}
+    
+}
+
+/* Zeile 3: variable erstellt
+
+Zeile 5 mit dem Begriff while eine Schleife erstelllt in den kammern (coundwon (Variablen name) weniger < 100)
+Zeile 7: variablen name++ daruch wird immer plus eines gezählt  
+
+*/
